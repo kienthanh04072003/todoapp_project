ca1b674d69c1a2e817a71ca36b567c97f92e69a5
kienthanh04072003 <kienthanh04072003@gmail.com>
2025-10-24 17:56:07 +0700

    deploy now

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -3,20 +3,34 @@ import tasksRoutes from "./routes/tasksRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
 const app = express();
 
 //middle ware
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173"})); // cho phep cac ung dung o domain khac co the truy cap du lieu tu server nay
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({origin: "http://localhost:5173"}));
+}// cho phep cac ung dung o domain khac co the truy cap du lieu tu server nay
 
 app.use(express.json()); // middleware chuyen doi du lieu gui len thanh dang json
 
 
 app.use("/api/task", tasksRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../FrontEnd/dist"))); // phuc vu cac file tinh tu thu muc dist
+
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../FrontEnd/dist/index.html"));
+})
+}
 
 // dung nhu nay de khi ket noi csdl thanh cong the sever moi duoc hoat dong neu ket noi khong duoc thi se khong the khoi dong sv
 connectDB().then(()=>{
